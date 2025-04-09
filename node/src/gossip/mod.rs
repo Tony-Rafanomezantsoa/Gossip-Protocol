19550ac3d7bd6c5adbe6dfb178b08e031b30530e
Tony Rafanomezantsoa <tony.mandresy@gmail.com>
2025-04-09 19:59:36 +0300

    Update GossipRequest::parse() function

--- node/src/gossip/mod.rs
+++ node/src/gossip/mod.rs
@@ -5,6 +5,7 @@ pub(crate) mod protocol;
 
 /// Represents the data to disseminate
 /// using the Gossip protocol.
+#[derive(PartialEq, Eq, Debug)]
 pub(crate) struct State {
     pub data: String,
     pub timestamp: u128,
--- node/src/gossip/protocol.rs
+++ node/src/gossip/protocol.rs
@@ -9,6 +9,7 @@ use super::State;
 
 /// Request abstraction for
 /// the Gossip protocol.
+#[derive(PartialEq, Eq, Debug)]
 pub(crate) enum GossipRequest {
     UpdateData(String),
     ShareData(Option<State>),
@@ -23,6 +24,20 @@ impl GossipRequest {
             let request_datas = update_data_request_regex.captures(request).unwrap();
             let data = request_datas[1].to_string();
             return Ok(Self::UpdateData(data));
+        }
+
+        // SHARE_DATA text protocol parsing
+        if request == "SHARE_DATA=NONE;" {
+            return Ok(Self::ShareData(None));
+        }
+
+        let share_data_request_regex = Regex::new(r"^SHARE_DATA=\[(.+)\]\[([0-9]+)\];$").unwrap();
+
+        if share_data_request_regex.is_match(request) {
+            let request_datas = share_data_request_regex.captures(request).unwrap();
+            let data = request_datas[1].to_string();
+            let timestamp = request_datas[2].parse::<u128>().unwrap();
+            return Ok(Self::ShareData(Some(State { data, timestamp })));
         }
 
         Err(From::from("invalid request (protocol error)"))
@@ -41,6 +56,29 @@ mod gossip_request_protocol_test {
 
         if let GossipRequest::UpdateData(data) = gossip_request {
             assert_eq!(data, String::from("Some data ..."));
+        } else {
+            panic!("parsing error");
+        }
+    }
+
+    #[test]
+    fn share_data_request_parse_test() {
+        // SHARE_DATA request protocol with NONE
+        let request = "SHARE_DATA=NONE;";
+
+        let gossip_request = GossipRequest::parse(request).unwrap();
+
+        assert_eq!(gossip_request, GossipRequest::ShareData(None));
+
+        // SHARE_DATA request protocol with DATA
+        let request = "SHARE_DATA=[Some data ...][7851391275623];";
+
+        let gossip_request = GossipRequest::parse(request).unwrap();
+
+        if let GossipRequest::ShareData(data) = gossip_request {
+            let data = data.unwrap();
+            assert_eq!(data.data, String::from("Some data ..."));
+            assert_eq!(data.timestamp, 7851391275623);
         } else {
             panic!("parsing error");
         }
